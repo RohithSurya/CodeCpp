e946209ef7581466d6418fd6164d55252f10cc21
Podugu Rohith Surya <prsurya1020@gmail.com>
2021-01-27 17:22:55 +0530

    Brute force and kmp substring algorithm

---
+++ strings/search/kmp_search.cpp
@@ -0,0 +1,142 @@
+/**
+ * @author: Podugu Rohith Surya
+ * @link: Link
+ * @desc: Write something about problem
+ * Input:
+ * Output:
+ **/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define gc getchar_unlocked
+#define fo(i, n) for (__typeof(n) i = 0; i < n; i++)
+#define Fo(i, k, n) for (__typeof(n) i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define si(x) scanf("%d", &x)
+#define sl(x) scanf("%lld", &x)
+#define ss(s) scanf("%s", s)
+#define pi(x) printf("%d\n", x)
+#define pl(x) printf("%lld\n", x)
+#define ps(s) printf("%s\n", s)
+#define deb(x) cout << #x << "=" << x << endl
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int mpow(int base, int exp);
+void ipgraph(int n, int m);
+void dfs(int u, int par);
+
+const int mod = 1'000'000'007;
+const int N = 3e5, M = N;
+
+int n, m;
+
+vi g[N];
+int a[N];
+
+vi computePrefix(string p) {
+  vi prefix(p.length());
+  prefix[0] = 0;
+  int j = 0;
+  Fo(i, 1, p.length()) {
+    if (p[i] == p[j]) {
+      prefix[i] = j + 1;
+      j++;
+    } else {
+      while (j > 0 && p[i] != p[j]) {
+        j = prefix[j - 1];
+      }
+      if (p[i] == p[j]) {
+        prefix[i] = j + 1;
+        j++;
+      } else
+        prefix[i] = 0;
+    }
+  }
+  return prefix;
+}
+
+void solve() {
+  string t, p;
+  cin >> p;
+  cin >> t;
+  vi prefix = computePrefix(p);
+  int j = 0, i = 0, cnt = 0;
+  while (i < t.length()) {
+    if (t[i] == p[j]) {
+      i++;
+      j++;
+    } else {
+      if (j != 0) {
+        j = prefix[j - 1];
+      } else {
+        i++;
+      }
+    }
+    if (j == p.length()) {
+      cnt++;
+      j = prefix[j - 1];
+    }
+  }
+  cout << cnt << endl;
+  // tr(it, prefix) cout << *it << " ";
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+  ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  int t = 1;
+  // cin >> t;
+  while (t--) {
+    solve();
+  }
+}
+
+int mpow(int base, int exp) {
+  base %= mod;
+  int result = 1;
+  while (exp > 0) {
+    if (exp & 1)
+      result = ((ll)result * base) % mod;
+    base = ((ll)base * base) % mod;
+    exp >>= 1;
+  }
+  return result;
+}
+
+void ipgraph(int n, int m) {
+  int i, u, v;
+  while (m--) {
+    cin >> u >> v;
+    u--, v--;
+    g[u].pb(v);
+    g[v].pb(u);
+  }
+}
+
+void dfs(int u, int par) {
+  for (int v : g[u]) {
+    if (v == par)
+      continue;
+    dfs(v, u);
+  }
+}
